00302243a4fcc3004fa18fc3d923ea68be6420e0
Camilo_Contreras-MFS <75485698+concamil-globant@users.noreply.github.com>
2022-03-24 12:10:08 -0300

    initial load.

---
+++ newsapp/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:newsapp/src/pages/tabs_pages.dart';
+import 'package:newsapp/src/theme/my_theme.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      theme: myTheme,
+      home: const TabsScreen(),
+    );
+  }
+}
---
+++ newsapp/lib/src/theme/my_theme.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+final myTheme = ThemeData.dark().copyWith(colorScheme: ThemeData().colorScheme.copyWith(secondary: Colors.red));
