7197790c967d56968f5507d97ccc8d646f8c45b1
Camilo_Contreras-MFS <75485698+concamil-globant@users.noreply.github.com>
2022-04-28 18:41:47 -0400

    book searching

---
+++ dynamic_listview/lib/book.dart
@@ -0,0 +1,15 @@
+class Book {
+  final String fullName;
+  final String price;
+
+  Book(this.fullName, this.price);
+}
+
+List<Book> retrieveAllBooks = [
+  Book('Primer Libro', 'Primer Precio'),
+  Book('Segundo Libro', 'Segundo Precio'),
+  Book('Tercer Libro', 'Tercer Precio'),
+  Book('Cuarto Libro', 'Cuarto Precio'),
+  Book('Quinto Libro', 'Quinto Precio'),
+  Book('Sexto Libro', 'Sexto Precio')
+];
---
+++ dynamic_listview/lib/listview.dart
@@ -0,0 +1,60 @@
+import 'package:dynamic_listview/book.dart';
+import 'package:flutter/material.dart';
+
+class ListViewScreen extends StatefulWidget {
+  const ListViewScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ListViewScreen> createState() => _ListViewScreenState();
+}
+
+class _ListViewScreenState extends State<ListViewScreen> {
+  List<Book> options = retrieveAllBooks;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Lista de Elementos'),
+      ),
+      body: Column(children: [
+        Container(
+          margin: const EdgeInsets.all(10.0),
+          child: TextField(
+              decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.search), hintText: 'Busca Acá'),
+              onChanged: filterBooks),
+        ),
+        Expanded(
+            child: ListView.builder(
+                itemCount: options.length,
+                itemBuilder: (context, index) {
+                  final book = options[index];
+
+                  return ListTile(
+                    title: Text(book.fullName),
+                  );
+                }))
+      ]),
+    );
+  }
+
+  void filterBooks(String value) {
+    if (value.isEmpty) {
+      setState(() {
+        options = retrieveAllBooks;
+      });
+      return;
+    }
+
+    final suggestions = options.where((element) {
+      final billTitle = element.fullName.toLowerCase();
+      final input = value.toLowerCase();
+      return billTitle.contains(input);
+    }).toList();
+
+    setState(() {
+      options = suggestions;
+    });
+  }
+}
